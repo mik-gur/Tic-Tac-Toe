b8d3c251b111a70ad1e6839605c0026e482044ff
Mikołaj Gurgul <mik.gurgul@gmail.com>
2022-07-16 10:33:10 +0200

    BoardGame

---
+++ src/main/java/org/example/BoardGame.java
@@ -0,0 +1,28 @@
+package org.example;
+
+public class BoardGame {
+    private final int SIZE_BOARD = 3;
+
+    public char[][] createBoardGame(){
+        char[][] board = new char[SIZE_BOARD][SIZE_BOARD];
+        for(int i = 0; i < SIZE_BOARD; i++) {
+            for (int j = 0; j < SIZE_BOARD; j++) {
+                board[i][j] = '-';
+            }
+        }
+        return board;
+    }
+
+    public void drawBoard(char[][] board){
+        System.out.println("Board: \n");
+
+        for(int i = 0; i < board.length; i++){
+            System.out.print("|");
+            for(int j= 0; j <board[i].length; j++){
+                System.out.print(board[i][j]);
+                System.out.print("|");
+            }
+            System.out.println();
+        }
+    }
+}
